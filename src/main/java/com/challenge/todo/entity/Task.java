c8657a72231aaa9a53ba25ea632cd978e2b76ee5
MarVer003 <martin.versnjak@gmail.com>
2024-03-01 03:21:40 +0100

    getter and setter for id

--- src/main/java/com/challenge/todo/entity/Task.java
+++ src/main/java/com/challenge/todo/entity/Task.java
@@ -14,6 +14,15 @@ public class Task{
     private String description;
     private Boolean completed;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
